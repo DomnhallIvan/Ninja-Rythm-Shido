c80e18b808156edd9d7a674274998a9b977ddb2b
ALau00 <71567281+ALau00@users.noreply.github.com>
2023-03-14 12:58:44 -0600

    Yeah Boiiiiii

--- Assets/Ninja_Rythm/Scripts/Scripts/NoteController.cs
+++ Assets/Ninja_Rythm/Scripts/Scripts/NoteController.cs
@@ -3,20 +3,44 @@ using UnityEngine;
 public class NoteController : MonoBehaviour
 {
     //private GameController gameController;
+    public NoteScoreManager noteScoreManager;
 
     void Start()
     {
-     //   gameController = GameObject.FindObjectOfType<GameController>();
+        noteScoreManager = GameObject.FindObjectOfType<NoteScoreManager>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
         // Si la nota colisiona con el jugador, destrúyela
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerEX"))
         {
-            Debug.Log("Entre en colison");
+            Debug.Log("Entre en colison Excelente");
+            noteScoreManager.ChangeExScore(100);
             Destroy(gameObject);
            // gameController.Score();
         }
+        else if (other.CompareTag("PlayerG"))
+        {
+            Debug.Log("Entre en colison good");
+            noteScoreManager.ChangeExScore(75);
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+        else if (other.CompareTag("PlayerMeh"))
+        {
+            Debug.Log("Entre en colison meh");
+            noteScoreManager.ChangeExScore(50);
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+        else if (other.CompareTag("PlayerBd"))
+        {
+            Debug.Log("Entre en colison  bad");
+            noteScoreManager.ChangeExScore(25);
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+
     }
 }
