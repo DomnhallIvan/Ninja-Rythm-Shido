7a9d4a754181c37e6b6cad8815eb973a3961769d
Ivan PM <cdmx2125@amerike.edu.mx>
2023-04-04 18:09:22 -0600

    Algunas Cosas Importadas

--- Assets/Ninja_Rythm_Elliot/Scripts/NoteController.cs
+++ Assets/Ninja_Rythm_Elliot/Scripts/NoteController.cs
@@ -3,20 +3,51 @@ using UnityEngine;
 public class NoteController : MonoBehaviour
 {
     //private GameController gameController;
+    public NoteScoreManager noteScoreManager;
+    public List_Of_Effects Efectos;
+
 
     void Start()
     {
-     //   gameController = GameObject.FindObjectOfType<GameController>();
+        noteScoreManager = GameObject.FindObjectOfType<NoteScoreManager>();
+        Efectos = GameObject.FindObjectOfType<List_Of_Effects>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
         // Si la nota colisiona con el jugador, destrúyela
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("PlayerEX"))
         {
-            Debug.Log("Entre en colison");
+            Efectos.DeluxyCombo(1);
+            Efectos.NoteStatus.text = "Excelent";
+            noteScoreManager.ChangeExScore(100);
             Destroy(gameObject);
            // gameController.Score();
         }
+        else if (other.CompareTag("PlayerG"))
+        {
+            Efectos.DeluxyCombo(1);
+            noteScoreManager.ChangeExScore(75);
+            Efectos.NoteStatus.text = "Good";
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+        else if (other.CompareTag("PlayerMeh"))
+        {
+            Efectos.DeluxyCombo(1);
+            Efectos.NoteStatus.text = "Meh";
+            noteScoreManager.ChangeExScore(50);
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+        else if (other.CompareTag("PlayerBd"))
+        {
+            Efectos.DeluxyCombo(1);
+            Efectos.NoteStatus.text = "Bruh";
+            noteScoreManager.ChangeExScore(25);
+            Destroy(gameObject);
+            // gameController.Score();
+        }
+
     }
 }
---
+++ Assets/Ninja_Rythm_Elliot/Scripts/Scripts/List_Of_Effects.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class List_Of_Effects : MonoBehaviour
+{
+    
+    public Text ScoreText;
+    public Text NoteStatus;
+    public int PlayerComboScore;
+
+    public Image CoboImage;
+    public Sprite[] ComboStoneImage;
+
+    public GameObject[] Effects;
+
+
+    private void Start()
+    {
+        PlayerComboScore = 0;
+        CoboImage.sprite = ComboStoneImage[0];
+    }
+
+    public void DeluxyCombo(int Combo)
+    {
+        PlayerComboScore += Combo;
+        ScoreText.text = ("X " + PlayerComboScore);
+        if (PlayerComboScore>=200)
+        {
+            CoboImage.sprite = ComboStoneImage[6];
+        }
+        else if(PlayerComboScore>=100)
+        {
+            CoboImage.sprite=ComboStoneImage[5];
+        }
+        else if (PlayerComboScore >= 50)
+        {
+            CoboImage.sprite = ComboStoneImage[4];
+        }
+        else if (PlayerComboScore >= 25)
+        {
+            CoboImage.sprite = ComboStoneImage[3];
+        }
+        else if (PlayerComboScore >= 10)
+        {
+            CoboImage.sprite = ComboStoneImage[2];
+        }
+        else if (PlayerComboScore >= 5)
+        {
+            CoboImage.sprite = ComboStoneImage[1];
+        }
+        
+    }
+
+   /* public void WomboCombo(int Combo)
+    {
+        PlayerComboScore += Combo;
+        ScoreText.text = ("X " + PlayerComboScore);
+        if (PlayerComboScore >= 200) //Si tiene 5 en combo se activa de lo contrario se desactiva
+        {
+            Effects[0].gameObject.SetActive(true);
+            ComboText[0].gameObject.SetActive(true);
+        }
+        
+        if (PlayerComboScore >= 100 ) //Si tiene 10 en combo se activa de lo contrario se desactiva
+        {
+            
+            Effects[1].gameObject.SetActive(true);
+            ComboText[1].gameObject.SetActive(true);
+        }
+        
+        if (PlayerComboScore >= 50 )
+        {
+            Effects[2].gameObject.SetActive(true);
+            ComboText[2].gameObject.SetActive(true);
+        }
+        
+        if (PlayerComboScore == 25 )
+        { 
+            Effects[3].gameObject.SetActive(true);
+            ComboText[3].gameObject.SetActive(true);
+        }
+        
+        if (PlayerComboScore==10 )
+        {  
+            Effects[4].gameObject.SetActive(true);
+            ComboText[4].gameObject.SetActive(true);
+        }
+       
+        if (PlayerComboScore==5)
+        {        
+            Effects[5].gameObject.SetActive(true);
+            ComboText[5].gameObject.SetActive(true);
+        }
+       
+    }*/
+
+    public void Bruh()
+    {
+        PlayerComboScore = 0;
+        ScoreText.text = ("X " + PlayerComboScore);
+        
+            CoboImage.sprite = ComboStoneImage[7];
+        
+
+    }
+
+   
+
+    /*public void ActivateObjectAtIndex(Image[] ComboText, int Combo)
+    {
+        for (int i = 0; i < ComboText.Length; i++)
+        {
+            PlayerComboScore = PlayerComboScore + Combo;
+            ScoreText.text = ("X " + PlayerComboScore);
+            if (i == 5)
+            {
+                ComboText[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                ComboText[i].gameObject.SetActive(false);
+            }
+        }
+    }*/
+
+}
