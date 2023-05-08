cb8747431468ea3c0a8eed00a464433a716e72d9
MaxIb62 <cdmx2254@amerike.edu.mx>
2023-05-08 00:30:37 -0600

    sonido agarrar nota y botones/ui

---
+++ Assets/script/abrirnivel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class abrirnivel : MonoBehaviour
+{
+    public void CargarNivel(string Nombreniv)
+    {
+        SceneManager.LoadScene(Nombreniv);
+    }
+
+}
